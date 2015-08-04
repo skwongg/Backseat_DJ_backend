57083e45f8122a4b16d4db81099a9dc425749959
skwongg <silaskwong1@gmail.com>
2015-08-03 21:05:59 -0700

    fixed rack issue

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,11 @@
+# Fix the environment
+if ENV['STAGING']
+  %w(RACK_ENV RAILS_ENV).each do |key|
+    ENV[key] = 'staging'
+  end
+end
+
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
