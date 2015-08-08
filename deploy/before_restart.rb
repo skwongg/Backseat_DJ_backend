e0e5abc9890eac19dfd6de6ee5c6a67fab4b0d1a
skwongg <silaskwong1@gmail.com>
2015-08-08 12:57:05 -0700

    deployment pg fix 10

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,2 +1,2 @@
-# run "cd /srv/www/photopoll/current && /usr/local/bin/bundle exec rake assets:precompile"
-run "cd /srv/www/Backseat_DJ_backend/current && /usr/local/bin/bundle exec rake assets:precompile"
+# run "cd /srv/www/Backseat_DJ_backend/current && /usr/local/bin/bundle exec rake assets:precompile"
+run "cd /srv/www/photopoll/current && /usr/local/bin/bundle exec rake assets:precompile"
